fe9f81bb492d9e45f0dfc930eb53284c2f71a1fb
Seunghoon Oh <stdsalmon@gmail.com>
2019-05-09 10:50:45 +0900

    .

--- app/src/main/java/com/tistory/deque/widgetexample/MainActivity.kt
+++ app/src/main/java/com/tistory/deque/widgetexample/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //Conflict test 1
+        //Conflict test 2
         setContentView(R.layout.activity_main)
     }
 }
